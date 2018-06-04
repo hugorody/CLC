36f1eea5722a7bbcf554dba3b8043c39c0acc2b4
Hugo Rody <hugo.rody@unifesp.br>
2018-06-04 11:13:34 -0300

    Update clc2countData.py

--- clc2countData.py
+++ clc2countData.py
@@ -1,12 +1,33 @@
 #!/usr/bin/python3
 #script used to parse CLC outputs and create a count data input for DESeq2
 
-csvfiles = ["flowering_meristem_01.csv", "flowering_meristem_02.csv", "flowering_meristem_03.csv",
-            "iac_ctr_48h_01.csv", "iac_ctr_48h_02.csv", "iac_ctr_48h_03.csv",
-            "iac_inoc_48h_01.csv","iac_inoc_48h_02.csv", "iac_inoc_48h_03.csv",
-            "rb_whip_01.csv", "rb_whip_02.csv", "rb_whip_03.csv",
-            "sp_ctr_48h_01.csv", "sp_ctr_48h_02.csv", "sp_ctr_48h_03.csv",
-            "sp_inoc_48h_01.csv", "sp_inoc_48h_02.csv", "sp_inoc_48h_03.csv"]
+csvfiles = ["Flower_meristem_RB_01.csv", "Flower_meristem_RB_02.csv", "Flower_meristem_RB_03.csv",
+            "IAC_control-48h_01.csv", "IAC_control-48h_02.csv", "IAC_control-48h_03.csv",
+            "IAC_inoculation-48h_01.csv", "IAC_inoculation-48h_02.csv", "IAC_inoculation-48h_03.csv",
+            "SP_Control-48h_01.csv", "SP_Control-48h_02.csv", "SP_Control-48h_03.csv",
+            "SP_inoculation-48h_01.csv", "SP_inoculation-48h_02.csv", "SP_inoculation-48h_03.csv",
+            "Whip_RB_R1.csv", "Whip_RB_R2.csv", "Whip_RB_R3.csv"]
+
+
+acronyms={
+'Flower_meristem_RB_01.csv':'flmer1',
+'Flower_meristem_RB_02.csv':'flmer2',
+'Flower_meristem_RB_03.csv':'flmer3',
+'IAC_control-48h_01.csv':'iactr1',
+'IAC_control-48h_02.csv':'iactr2',
+'IAC_control-48h_03.csv':'iactr3',
+'IAC_inoculation-48h_01.csv':'iaino1',
+'IAC_inoculation-48h_02.csv':'iaino2',
+'IAC_inoculation-48h_03.csv':'iaino3',
+'SP_Control-48h_01.csv':'spctr1',
+'SP_Control-48h_02.csv':'spctr2',
+'SP_Control-48h_03.csv':'spctr3',
+'SP_inoculation-48h_01.csv':'spino1',
+'SP_inoculation-48h_02.csv':'spino2',
+'SP_inoculation-48h_03.csv':'spino3',
+'Whip_RB_R1.csv':'whipr1',
+'Whip_RB_R2.csv':'whipr2',
+'Whip_RB_R3.csv':'whipr3'}
 
 refcounts = {} #final dictionary
 
@@ -15,10 +36,9 @@ for j in csvfiles:
     with open(j,"r") as set1:
         for i in set1:
             i = i.rstrip()
-            i = i.split(",")
+            i = i.split(";")
             if "Name" not in i and "skipped" not in i:
-                ref = i[0]
-                #u_reads = i[6]
+                ref = i[0].replace("\"","")
                 if ref not in refcounts:
                     refcounts[ref] = []
 
@@ -30,9 +50,9 @@ for j in csvfiles:
     with open(j,"r") as set1:
         for i in set1:
             i = i.rstrip()
-            i = i.split(",")
+            i = i.split(";")
             if "Name" not in i and "skipped" not in i and "" not in i:
-                ref = i[0]
+                ref = i[0].replace("\"","")
                 u_reads = i[6].replace("\"","")
                 countsfile[ref] = u_reads
 
@@ -48,7 +68,7 @@ for j in csvfiles:
                 addreads.append(countsfile[jj[0]])
                 refcounts[jj[0]] = addreads
 
-        seqoffiles.append(j) #create a list of files to address the order they were read
+        seqoffiles.append(acronyms[j]) #create a list of files to address the order they were read
 
 outtable = open("DESeq2input.csv","w") #open output to write
 
